2cbf5d0818522eb84060bded700e4eadf05c5dd2
Henrik Hedlund Andersen <headwiki@pm.me>
2022-01-21 23:54:57 +0100

    actix web server with test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+//! main.rs
+
+use std::net::TcpListener;
+use ffxiv_hub::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    run(
+        TcpListener::bind("127.0.0.1:8080").
+            expect("Failed to bind random port"))
+        ?.await
 }
